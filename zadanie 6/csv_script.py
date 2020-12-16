ddeb6c0e0d79ea3ed13dc8c8ca18c7719c2e91e8
Mrozuu <m.jamrozik98@gmail.com>
2020-12-16 12:45:16 +0100

    commit

--- zadanie 6/csv_script.py
+++ zadanie 6/csv_script.py
@@ -22,7 +22,9 @@ while True:
     if choice == '2':
         file = open('bikes.csv', 'r', newline='')
         data = csv.reader(file)
-        row_count = sum(1 for row in data)
+        row_count = 0
+        for row in data:
+            row_count = int(row[0])
         file.close()
         row_id = row_count + 1
         name = input("Name: ")
@@ -34,14 +36,13 @@ while True:
     if choice == '3':
         file_r = open('bikes.csv', 'r', newline='')
         data = csv.reader(file_r)
-        id_row = str(input("Which record you want to delete? Enter id."))
+        id_row = str(input("Which record you want to delete? Enter id: "))
         status = False
         file_w = open('bikes_tmp.csv', 'w', newline='')
-        writer = csv.writer(file_w)
+        writer = csv.writer(file_w, delimiter=',')
         for row in data:
             if row[0] != id_row:
-                print("!" + row[0] )
-                writer.write(row)
+                writer.writerow([row[0], row[1], row[2]])
             else:
                 status = True
         file_r.close()
@@ -50,6 +51,12 @@ while True:
             print("Row was deleted")
             os.remove("bikes.csv")
             os.rename("bikes_tmp.csv", "bikes.csv")
+            with open('bikes.csv') as file:
+                lines = file.readlines()
+                last_line = lines[len(lines) - 1]
+                lines[len(lines) - 1] = last_line.rstrip()
+            with open('bikes.csv', 'w') as file:
+                file.writelines(lines)
         else:
             print("No record with entered ID")
 
